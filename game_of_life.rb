6e1a4994756b401ab51c4c4b8b6d10a908bbc67e
patrick <mixelpix@mac.com>
2016-09-18 01:14:31 -0400

    part3

--- game_of_life.rb
+++ game_of_life.rb
@@ -1,2 +1,12 @@
 # basic file
 
+class World
+  attr_accessor :rows, :cols        # These methods are now accessible to rspec
+  def initialize(rows=3, cols=3)    # default argument "=values"
+    @rows      = rows
+    @cols      = cols
+  end                               # These methods are now initialized
+
+end
+
+
--- spec_game_of_life.rb
+++ spec_game_of_life.rb
@@ -1,6 +1,23 @@
 # spec file
 
 require 'rspec'
+require_relative 'game_of_life.rb'
 
 describe 'Game of Life' do
+
+  context 'world' do
+    subject { World.new }
+
+    it 'should create a new world object' do
+      expect(subject.is_a?(World)).to be true
+    end
+    it 'should respond to proper methods' do
+      expect(subject.respond_to?(:rows))
+      expect(subject.respond_to?(:cols))
+    end
+
+  end
+
 end
+
+
