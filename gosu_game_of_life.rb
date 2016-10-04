f0322bca02e9fb8559db7154594ec731d428b8ab
patrick <mixelpix@mac.com>
2016-10-04 18:26:10 -0400

    part13

--- gosu_game_of_life.rb
+++ gosu_game_of_life.rb
@@ -1,2 +1,26 @@
 # Gosu file
 
+require 'gosu'
+require_relative 'game_of_life.rb'
+
+class GameOfLifeWindow < Gosu::Window
+
+  def initialize
+    super 640, 480, false
+    self.caption = "Conway's Game Of Life"
+  end
+
+  def update
+  end
+
+  def draw
+  end
+
+end
+
+
+#GameOfLifeWindow.new.show
+window = GameOfLifeWindow.new.show
+window.show
+
+
